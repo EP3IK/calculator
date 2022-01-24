c9259cf68bedbbce209db0ab0de092ad3a4b3609
hcpak <jasotn12@naver.com>
2022-01-24 18:14:13 +0900

    asdasd

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("수식을 입력해주세요. ([ex] 1 + 2): ");
+	printf("99989898989");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
